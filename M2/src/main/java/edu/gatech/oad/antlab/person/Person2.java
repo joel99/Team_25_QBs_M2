f7434ddb63a10adfaeb89ea4a6a4ea6da97a7499
Shiva Devarajan <sdevarajan3@gatech.edu>
2018-01-31 14:09:02 -0500

    Revert "Added resources.jar and deleted .class files"
    This reverts commit 5eab139ed5768bc9359807e389ecebcc091b911e.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,13 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Jihwan Kim
  * @version 1.1
  */
 public class Person2 {
@@ -29,9 +31,30 @@ public class Person2 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+	private static String calc(String input) {
+	    if (input == null) {
+		throw new IllegalArgumentException("The input is null string and cannot be put in random order.");
+	    } else {
+
+		Random random = new Random();
+
+		char charArrayInput[] = input.toCharArray();
+		
+		for (int i = 0; i < charArrayInput.length; i++) {
+		    int j = random.nextInt(charArrayInput.length);
+
+		    char temp = charArrayInput[i];
+		    charArrayInput[i] = charArrayInput[j];
+		    charArrayInput[j] = temp;
+		}
+
+		String randomOrderName = "";
+		for (int i = 0; i < charArrayInput.length; i++) {
+		    randomOrderName += charArrayInput[i];
+		}
+
+		return randomOrderName;
+	    }
 	}
 	/**
 	 * Return a string rep of this object
